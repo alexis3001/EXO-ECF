a4993488bc09ecf7c1e26bf113a54112c9869b8d
alexis3001 <alexisriviere3001@gmail.com>
2025-08-08 09:06:02 +0200

    jadore vercel

--- app/abo.réussi/page.js
+++ app/abo.réussi/page.js
@@ -1,7 +1,7 @@
 
 import { redirect } from 'next/navigation'
 
-import { stripe } from '../../lib/stripe'
+// import { stripe } from '../../lib/stripe'
 
 export default async function Success({ searchParams }) {
   const { session_id } = await searchParams
